bc3e2ca95d4f7578d3c92f069fee432333f008c6
Mijael Callejas <mijael.rcf@gmail.com>
2020-05-29 23:12:12 -0400

    creation header and footer components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [FooterComponent, HeaderComponent],
   imports: [
     CommonModule
   ]
